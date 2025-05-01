026e3a361afa6fa012892517a63c3e5b93f79b36
Mithun Gowda B <99024517+mithun50@users.noreply.github.com>
2025-05-01 08:52:56 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
+// src/App.tsx
+
 import React, { useEffect } from "react";
-import { BrowserRouter, Routes, Route, Navigate, useLocation  } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate, useLocation } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 import { CoursePage } from "./pages/CoursePage";
 import { LeaderboardPage } from "./pages/LeaderboardPage";
-import BackToTop from "./components/BackToTop";
 import { Blogs } from './pages/Blogs';
 import { BlogPost } from './pages/BlogPost';
+import { Header } from "./components/Header";
+import BackToTop from "./components/BackToTop";
 import * as ga from './utils/analytics';
 
-//Google analytics 
+// Google analytics 
 function AnalyticsWrapper({ children }: { children: React.ReactNode }) {
   const location = useLocation();
 
@@ -19,21 +22,33 @@ function AnalyticsWrapper({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
 }
 
-
+// Layout component to include Header with all routes
+function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Header />
+      <main>
+        {children}
+      </main>
+    </>
+  );
+}
 
 function App() {
   return (
     <BrowserRouter>
       <AnalyticsWrapper>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/courses" element={<HomePage />} />
-          <Route path="/course/:courseId" element={<CoursePage />} />
-          <Route path="/leaderboard" element={<LeaderboardPage />} />
-        <Route path="/blogs" element={<Blogs />} />
-        <Route path="/blogs/:slug" element={<BlogPost />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/courses" element={<HomePage />} />
+            <Route path="/course/:courseId" element={<CoursePage />} />
+            <Route path="/leaderboard" element={<LeaderboardPage />} />
+            <Route path="/blogs" element={<Blogs />} />
+            <Route path="/blogs/:slug" element={<BlogPost />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </Layout>
       </AnalyticsWrapper>
       <BackToTop />
     </BrowserRouter>
