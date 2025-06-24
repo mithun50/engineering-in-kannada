79ee11bd3fa7f1daa50ae259a3fa172d574b80b7
Mithun Gowda B <mithungowda.b7411@gmail.com>
2025-06-24 12:09:28 +0530

    Update CardSharePopover.tsx

--- src/components/CardSharePopover.tsx
+++ src/components/CardSharePopover.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Share2 } from 'lucide-react';
 import ShareButtons from './ShareButtons';
 import useOnClickOutside from '../utils/useOnClickOutside';
